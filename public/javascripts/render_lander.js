e5e92cf20b8146ce799d9005b5dfaeef5ae40fba
Mattias Petter Johansson <mpj@mpj.me>
2012-06-16 18:16:40 +0200

    Voodoo with gravitation and angles.

--- public/javascripts/render_lander.js
+++ public/javascripts/render_lander.js
@@ -11,12 +11,23 @@
 
   	var lander = CanvasApplication.lander;
 
-
+    // break this out into helper
+    ctx.setTransform(1, 0, 0, 1, 0, 0); // must reset transform before clearing.
   	ctx.clearRect(0, 0, CanvasApplication.STAGE_WIDTH, CanvasApplication.STAGE_HEIGHT);
 
     ctx.fillStyle = "rgb(255, 255, 255)";
-	  ctx.fillRect(lander.x, lander.y, 
-		CanvasApplication.LANDER_WIDTH, CanvasApplication.LANDER_HEIGHT);
+
+    var cx     = lander.x + 0.5 * CanvasApplication.LANDER_WIDTH;
+    var cy     = lander.y + 0.5 * CanvasApplication.LANDER_HEIGHT;
+    ctx.translate(cx, cy);
+    ctx.rotate( (Math.PI / 180) * lander.angle);
+
+	  ctx.fillRect(
+      -0.5 * CanvasApplication.LANDER_WIDTH, 
+      -0.5 * CanvasApplication.LANDER_HEIGHT, 
+		  CanvasApplication.LANDER_WIDTH, CanvasApplication.LANDER_HEIGHT);
+
+    ctx.setTransform(1, 0, 0, 1, 0, 0); 
 
 
   })
--- public/javascripts/update_lander.js
+++ public/javascripts/update_lander.js
@@ -6,11 +6,21 @@
     if (!CanvasApplication.lander)
       CanvasApplication.lander = {
         x: CanvasApplication.STAGE_WIDTH / 2 - CanvasApplication.LANDER_WIDTH / 2,
-        y: 100
+        y: 100,
+        angle: 40
       }
     
 
-    lander = CanvasApplication.lander;
+    lander = CanvasApplication.lander,
+    keysDown = CanvasApplication.keysDown;
+
+    if (37 in keysDown) { // PlayerB holding left
+      lander.angle -= 100 * modifier;
+    }
+    if (39 in keysDown) { // PlayerB holding right
+      lander.angle += 100 * modifier;
+    }
+
 
     lander.y += 35 * modifier;
 
